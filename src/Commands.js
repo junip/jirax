cc10f80cda7b89a158fc50e1d620cb5c9e5adc13
Junip Dewan <junipd2@gmail.com>
2019-05-30 00:58:18 +0530

    minor changes for commands

--- src/Commands.js
+++ src/Commands.js
@@ -2,74 +2,41 @@
 /**
  * After adiing this link add few changes in package.json file
  * "preferGlobal": true,
-  "bin": "./lib/Commands.js",
+  "bin": "./src/Commands.js",
    bin prefers to the current file name from directly we can initiate the command
  */
 
 /**
  * All Cli commands goes here
- *
  * `https://github.com/tj/commander.js/`
  *  CLI argument processing tool
  */
 
 const program = require("commander");
-const question = require("./api/Questions");
-const print = require("./api/Console");
 const input = require("./SaveInput");
 const issue = require("./api/IssueClient");
 const tablularPrint = require("./api/PrintDetails");
-
-const Configstore = require("configstore");
-const configStore = new Configstore("jiraconfig");
-
-var Table = require("cli-table3");
-var argv = require("minimist")(process.argv.slice(2));
 
 // jira-cli --verrsion
 program.version("1.0.0").description("Cli tool for JIRA");
 
 // jira-cli login
 program
-  .command("login")
-  .description("Login using API TOKEN")
-  .action(() => {
-    // prompt question
-    // verify and save
-    let isLoggedIn = configStore.get("encodedString");
-
-    if (!isLoggedIn) {
-      question.askCredential().then(answers => {
-        input.verifyAndSave(answers);
-      });
-    } else {
-      print.printInfo("YOU ARE ALREADY LOGGEDIN");
-    }
-  });
-
-// find ISSUE
-program
-  .command("open-issue")
-  .description("Browse the  issue")
-  .action(() => {
-    let key = argv._[1];
-    if (!key.length) {
-      print.printError("Please enter the issue number");
-    } else {
-      issue.openIssue(key);
-    }
-  });
-// issue details
-program
-  .command("issue-detail")
-  .description("Prints the basic issue details")
-  .action(() => {
-    let key = argv._[1];
-    if (!key.length) {
-      print.printError("Please enter the issue number");
-    } else {
-      tablularPrint.printIssueDetails({ issueKey: key });
-    }
-  });
+  .option("-l, --login", "Login Using JIRA API Token")
+  .option("-open, --open-issue <key>", "Open Issue Using KEYS for Given Key")
+  .option(
+    "-detail, --issue-detail <key>",
+    "Prints Issue Details for Given Key"
+  );
 
 program.parse(process.argv);
+
+if (program.login) {
+  input.signUpUser();
+}
+if (program.issueDetail) {
+  tablularPrint.printIssueDetails({ issueKey: program.issueDetail });
+}
+if (program.openIssue) {
+  issue.openIssue(program.openIssue);
+}
--- src/SaveInput.js
+++ src/SaveInput.js
@@ -7,17 +7,30 @@ const Configstore = require("configstore");
 const jira = require("./Authentication");
 const print = require("./api/Console");
 const chalk = require("chalk");
+const question = require("./api/Questions");
+const configStore = new Configstore("jiraconfig");
 
 /**
  * Verify the user and save the object
  * @config object
  */
 module.exports = {
+  signUpUser: function() {
+    let isLoggedIn = configStore.get("encodedString");
+    if (!isLoggedIn) {
+      question.askCredential().then(answers => {
+        module.exports.verifyAndSave(answers);
+      });
+    } else {
+      print.printInfo("You are already logged in");
+    }
+  },
+
   verifyAndSave: function(config) {
     // authenticate and save the inputs in config store
     jira.authenticate(config, function(data) {
       if (data.error) {
-        print.printError("Un authorized");
+        print.printError("Unauthorized - Please re-enter your credentials");
       } else {
         module.exports.storeInfo(data);
       }
