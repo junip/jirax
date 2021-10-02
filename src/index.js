ae90d02e58a2948145fa3a97f1797a223535540a
Junip Dewan <junipd2@gmail.com>
2021-10-02 20:07:21 +0530

    Added subcommands for the updates

--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
 #!/usr/bin/env node
 
-const program = require("commander");
-const input = require("./store");
+const program = require('commander');
+const input = require('./store');
 
-const { loadBoardCommands } = require("./commands/boards");
+const { loadBoardCommands } = require('./commands/boards');
 
-program.version("1.0.0").description("CLI Tool for accessing JIRA");
-program.option("-l, --login", "Login Using JIRA API Token");
-program.option("-c, --clear", "Clear your login credentials");
-program.parse();
+program.version('1.0.0').description('CLI Tool for accessing JIRA');
+program
+    .command('login')
+    .description('Login using JIRA API Token')
+    .action(() => {
+      input.signUpUser();
+    });
+
+program
+    .command('logout')
+    .description('Remove you JIRA credentials')
+    .action(() => {
+      input.removeCredentials();
+    });    
 
 // Add other sub commands here
-loadBoardCommands();
-
-// process optional commands here
-let currentCommand = Object.keys(program.opts())[0];
-switch (currentCommand) {
-  case "login":
-    input.signUpUser();
-    break;
-  case "-l":
-    input.signUpUser();
-    break;
-  case "clear":
-    input.removeCredentials();
-    break;
-  default:
-    break;
-}
+loadBoardCommands()
+program.parse(process.argv)
